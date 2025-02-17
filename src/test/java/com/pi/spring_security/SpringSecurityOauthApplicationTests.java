20edc53b87f8ea20840c45c10e9cdb8f48ccf003
Aranganathan <95405959+AranganathanM@users.noreply.github.com>
2025-02-17 13:32:42 +0530

    Spring Security and OAuth project Initialize

---
+++ src/test/java/com/pi/spring_security/SpringSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
